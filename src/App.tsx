b305f8a2f74ab3045fb41770e57b045fc1619987
Romberto <korhoff@mail.ru>
2025-02-10 13:28:39 +0400

    reserch

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./components/page/Home/Home";
-import { AddReceptModul } from "./components/UI/AddReceptModal/AddReceptModul";
 import { PATH } from "./utils/constans";
 
 function App() {
@@ -8,7 +7,6 @@ function App() {
   <div className="container">
     <Routes>
       <Route path={PATH.HOME} element={<Home/>}/>
-      <Route path={PATH.ADDRECEPTMOMAL} element={<AddReceptModul/>}/>
     </Routes>
     
 
